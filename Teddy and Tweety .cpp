dfb921b3a60a04ff5e8a06e37561a1885d2f20c9
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-09 20:11:19 +0530

    Create Teddy and Tweety .cpp

---
+++ Teddy and Tweety .cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;						
+	if(num % 3 == 0)				// Reading input from STDIN
+	cout << "YES" << endl;
+	else
+	cout<< "NO"<<endl;		// Writing output to STDOUT
+}
+
