7ef258c319bc4021b573ee67f7a02bb20ffc1762
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-09 20:29:09 +0530

    Create Doctor's Secret.cpp

---
+++ Doctor's Secret.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,l;
+	cin >> n>>l;
+	
+	if(n <= 23 && l <= 1000)	
+	cout<<"Take Medicine"<<endl;
+	
+	else 
+	cout<<"Don't take Medicine"<<endl;						// Reading input from STDIN
+	                                    
+											// Writing output to STDOUT
+}
