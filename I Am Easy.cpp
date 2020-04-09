f37703a44b89c55b0c04529605f927fc98a57fca
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-09 20:16:31 +0530

    Create I Am Easy.cpp

---
+++ I Am Easy.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;										// Reading input from STDIN
+	
+	for(int i=1;i<11;i++){
+		cout<<num*i<<endl;
+	}		// Writing output to STDOUT
+}
+
