264e71c3b0b7c9fb4f0466728157dbd4adca2e95
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-10 14:00:28 +0530

    Create Back to School.cpp

---
+++ Back to School.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin>>a>>b>>c;
+
+	if(a>b && a>c)
+	cout<<a;
+	if(b>c && b>a)
+	cout<<b;
+	if(c>b && c>b)
+	cout<<c;
+	
+
+
+}
+
