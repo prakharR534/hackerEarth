5b09995c93871b57a4473980cd7ee8830b13645a
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-16 21:12:33 +0530

    Create Find me!.cpp

---
+++ Find me!.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,a,b,c;
+	cin >> n>>a>>b;										// Reading input from STDIN
+	cout << n-a-b << endl;		// Writing output to STDOUT
+}
+
