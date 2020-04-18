560e42d977e708efb294caafef4b71c5604baf7f
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-04-19 00:34:06 +0530

    Create Monk Takes a Walk.cpp

---
+++ algorithm/Monk Takes a Walk.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;										// Reading input from STDIN
+	
+	while(num--){
+		//char A[100000];
+		string A;
+		int count=0;
+		cin>>A;
+
+		for( int i=0;i< A.length();i++){
+			if(A[i] == 'A' || A[i] == 'E' ||A[i] == 'I' ||A[i] == 'O' ||A[i] == 'U' ||A[i] == 'a' || A[i] == 'e' ||A[i] == 'i' ||A[i] == 'o' ||A[i] == 'u'  )
+			count++;
+		}
+		cout<<count<<endl;
+	}	// Writing output to STDOUT
+}
+
+
