a9b0e6839e3e4f1228dd7729e9e28cffc0bef4c2
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-05-23 12:54:38 +0530

    Update Find me!.cpp

--- Find me!.cpp
+++ Find me!.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main() {
 	int n,a,b,c;
-	cin >> n>>a>>b;										// Reading input from STDIN
+	cin >> n>>a>>b;
+	// Reading input from STDIN
 	cout << n-a-b << endl;		// Writing output to STDOUT
 }
 
