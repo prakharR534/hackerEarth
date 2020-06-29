d01c9f1c3c267226e2a5bc19160de8c1b210a376
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2020-06-29 23:59:36 +0530

    Update Back to School.cpp

--- Back to School.cpp
+++ Back to School.cpp
@@ -6,13 +6,10 @@ using namespace std;
 int main() {
 	int a,b,c;
 	cin>>a>>b>>c;
-
 	if(a>b && a>c)
-	cout<<a;
-	
+	cout<<a;	
 	if(b>c && b>a)
-	cout<<b;
-	
+	cout<<b;	
 	if(c>b && c>b)
 	cout<<c;
 	
